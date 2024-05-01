7fa14f5d820e2e49fa57d6fda9378851267556e2
Isaac Madrid <isaac.321748@gmail.com>
2024-04-30 23:18:07 -0600

    Program prompts user for first & last name then displays it accordingly.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         // Console.WriteLine("Prep1 Assignment:");
         Console.Write("What is your first name? "); // First Name.
         string f_name = Console.ReadLine(); // "f_name" stores previous input-^
-        Console.Write("What is your first name? "); // Last Name.
+        Console.Write("What is your last name? "); // Last Name.
         string l_name = Console.ReadLine(); // "l_name" stores previous input-^
         Console.WriteLine(); // spacer
         Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}."); 
