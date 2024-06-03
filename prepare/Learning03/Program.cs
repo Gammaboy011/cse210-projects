a4080178365547dfa6602adc65e8030fb27e532d
Isaac Madrid <isaac.321748@gmail.com>
2024-06-03 11:32:41 -0600

    Moved to "Learning 03" file.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -18,5 +18,12 @@ class Program {
         Fraction f4 = new Fraction(1, 3);
         Console.WriteLine(f4.GetFractionString());
         Console.WriteLine(f4.GetDecimalValue());
+        CashDrawer myCashDrawer = new CashDrawer();
+        // customer paid for $17.72 purchase with a twenty dollar bill:
+        myCashDrawer.Update(8, 1);
+        // customer received $2.28 in change:
+        myCashDrawer.Update(0, -3);
+        myCashDrawer.Update(3, -1);
+        myCashDrawer.Update(5, -2);
     }
 }
