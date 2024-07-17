1315a33ee490d0f136f8402382d7590cbf93d4fb
Isaac Madrid <isaac.321748@gmail.com>
2024-07-16 19:25:26 -0600

    Updated code and created a class for Json to save polymorfic files that way they can be saved and accessed appropriately.

--- final/FinalProject/Creature.cs
+++ final/FinalProject/Creature.cs
@@ -1,15 +1,14 @@
 using System;
+using System.Text;
 using System.Text.Json;
-public class Creature  { // Abstract base class representing a Creature
+using System.Text.Json.Serialization;
+public abstract class Creature  { // Abstract base class representing a Creature
     private string _name { get;set; } // Name of the Creature
-    public string GetName() {return _name;}
-    private string _descript { get;set; } // Description of the Creature
-    public string GetDescript() {return _descript;}
+    private string _descript { get;set; } // Description of the Creature    
     private float _health { get;set; } // Health of the Creature 
-    public float GetHealth() {return _health;}
     private float _stamina { get;set; } // Stamina of the Creature
-    public float GetStamina(){return _stamina;}
     private string _responsibilityType { get;set; } // Responsibilty type of the Creature
+
     // private Move[] _moves; // Array of moves available to the Creature
     public Creature(string name, string descript, float health, float stamina, string responsibilityType) { // Constructor to initialize a Creature with its attributes
         _name = name;
@@ -18,6 +17,28 @@ public class Creature  { // Abstract base class representing a Creature
         _stamina = stamina;
         _responsibilityType = responsibilityType;
     }
+    public string GetName() {return _name;}
+    public string GetDescript() {return _descript;}
+    public float GetHealth() {return _health;}
+    public float GetStamina(){return _stamina;}
+    public string GetResponsibilityType() {return _responsibilityType;}
+    public abstract void DisplayDetails();
+    public abstract void SaveCreature(string filePath);
+
+
+    // Static method to load a Creature's data from a file in JSON format.
+    public static Creature LoadCreature(string filePath)
+    {
+        var options = new JsonSerializerOptions
+        {
+            Converters = { new CreatureConverter() },
+            WriteIndented = true
+        };
+
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<Creature>(json, options);
+    }
+
     // Method to reduce the Creature's health by a specified amount of damage.
     public void TakeDamage(float damage) {
         _health -= damage;
@@ -32,25 +53,5 @@ public class Creature  { // Abstract base class representing a Creature
             Console.WriteLine("Not enough stamina.");
         }
     }
-    // Method to save the Creature's data to a file in JSON format.
-    public void SaveCreature(string filePath) {
-        var json = JsonSerializer.Serialize(this);
-        File.WriteAllText(filePath, json);
-        Console.WriteLine("Creature Saved.");
-    }
-    // Static method to load a Creature's data from a file in JSON format.
-    public static Creature LoadCreature(string filePath)
-    {
-        if (File.Exists(filePath))
-        {
-            var json = File.ReadAllText(filePath);
-            Console.WriteLine("Creature loaded.");
-            return JsonSerializer.Deserialize<Creature>(json);
-        }
-        else
-        {
-            Console.WriteLine("File not found.");
-            return null;
-        }
-    }
+
 }
---
+++ final/FinalProject/JsonConverter.cs
@@ -0,0 +1,32 @@
+public class CreatureConverter : JsonConverter<Creature>
+{
+    public override Creature Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using (var jsonDoc = JsonDocument.ParseValue(ref reader))
+        {
+            var root = jsonDoc.RootElement;
+            var type = root.GetProperty("ResponsibilityType").GetString();
+
+            switch (type.ToLower())
+            {
+                case "ranged":
+                    return JsonSerializer.Deserialize<Ranged>(root.GetRawText(), options);
+                case "melee":
+                    return JsonSerializer.Deserialize<Melee>(root.GetRawText(), options);
+                case "heavy":
+                    return JsonSerializer.Deserialize<Heavy>(root.GetRawText(), options);
+                case "support":
+                    return JsonSerializer.Deserialize<Support>(root.GetRawText(), options);
+                case "mystic":
+                    return JsonSerializer.Deserialize<Mystic>(root.GetRawText(), options);
+                default:
+                    throw new JsonException($"Unknown creature type: {type}");
+            }
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, Creature value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize(writer, (object)value, value.GetType(), options);
+    }
+}
