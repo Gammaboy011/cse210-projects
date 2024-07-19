cab6b2151c46a2cb8b741da0dda336f9ee40db9b
Isaac Madrid <isaac.321748@gmail.com>
2024-07-19 13:12:02 -0600

    Reformated code to take in account the moves for each creature

--- final/FinalProject/JsonConverter.cs
+++ final/FinalProject/JsonConverter.cs
@@ -3,32 +3,37 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-public class CreatureConverter : JsonConverter<Creature> {
-    public override Creature Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-        using (var jsonDoc = JsonDocument.ParseValue(ref reader)) {
-            var root = jsonDoc.RootElement;
-            var type = root.GetProperty("ResponsibilityType").GetString();
-            switch (type.ToLower()) {
-                case "ranged":
-                    return JsonSerializer.Deserialize<Ranged>(root.GetRawText(), options);
-                case "melee":
-                    return JsonSerializer.Deserialize<Melee>(root.GetRawText(), options);
-                case "heavy":
-                    return JsonSerializer.Deserialize<Heavy>(root.GetRawText(), options);
-                case "support":
-                    return JsonSerializer.Deserialize<Support>(root.GetRawText(), options);
-                case "mystic":
-                    return JsonSerializer.Deserialize<Mystic>(root.GetRawText(), options);
-                default:
-                    throw new JsonException($"Unknown creature type: {type}");
-            }
-        }
+// A custom converter for the Creature class to handle JSON serialization and deserialization
+public static class JsonConverter {
+    public static void SaveToJson<T>(T obj, string filePath) {
+        var options = new JsonSerializerOptions {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var json = JsonSerializer.Serialize(obj, options);
+        File.WriteAllText(filePath, json);
     }
 
-    public override void Write(Utf8JsonWriter writer, Creature value, JsonSerializerOptions options)
-    {
-        JsonSerializer.Serialize(writer, (object)value, value.GetType(), options);
+    public static T LoadFromJson<T>(string filePath) {
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<T>(json);
+    }
+
+    public static Creature CreateCreature(string responsibilityType, string name, string description, float health, float stamina, int meleeAgility = 0, int strength = 0, int defense = 0, int mana = 0, int agility = 0, int dexterity = 0, int healing = 0) {
+        return responsibilityType switch {
+            "heavy" => new Heavy(name, description, health, stamina, responsibilityType, defense),
+            "melee" => new Melee(name, description, health, stamina, responsibilityType, meleeAgility, strength),
+            "mystic" => new Mystic(name, description, health, stamina, responsibilityType, mana),
+            "ranged" => new Ranged(name, description, health, stamina, responsibilityType, agility, dexterity),
+            "support" => new Support(name, description, health, stamina, responsibilityType, healing),
+            _ => throw new ArgumentException("Invalid type. Valid types are: heavy, melee, mystic, ranged, support.")
+        };
     }
 }
 
+// Placeholder for a custom converter for the Profile class (currently not implemented)
 // public class ProfileConverter : JsonConverter<Profile> {}
+
+// Placeholder for a custom converter for the Moves class (currently not implemented)
+// public class MoveConverter : JsonConverter<Move> {}
